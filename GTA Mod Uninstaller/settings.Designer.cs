4931e29f9c1beb438f737cdf4bfb97f30e93b4f2
hesa656 <48819648+hesa656@users.noreply.github.com>
2020-04-13 15:30:35 +0200

    Add files via upload

---
+++ GTA Mod Uninstaller/settings.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace GTA_Mod_Uninstaller
+{
+    partial class settings
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.back_bttn = new System.Windows.Forms.PictureBox();
+            this.gtadir_txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.copyto_txt = new System.Windows.Forms.TextBox();
+            this.select_bttn = new System.Windows.Forms.Button();
+            this.select2_bttn = new System.Windows.Forms.Button();
+            this.reset_app_bttn = new System.Windows.Forms.Button();
+            this.panel_main = new System.Windows.Forms.Panel();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog_temp = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.back_bttn)).BeginInit();
+            this.panel_main.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // back_bttn
+            // 
+            this.back_bttn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.back_bttn.Image = global::GTA_Mod_Uninstaller.Properties.Resources.icons8_Back_64px;
+            this.back_bttn.Location = new System.Drawing.Point(3, 3);
+            this.back_bttn.Name = "back_bttn";
+            this.back_bttn.Size = new System.Drawing.Size(25, 25);
+            this.back_bttn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.back_bttn.TabIndex = 17;
+            this.back_bttn.TabStop = false;
+            this.back_bttn.Click += new System.EventHandler(this.back_bttn_Click);
+            // 
+            // gtadir_txt
+            // 
+            this.gtadir_txt.Location = new System.Drawing.Point(0, 34);
+            this.gtadir_txt.Name = "gtadir_txt";
+            this.gtadir_txt.Size = new System.Drawing.Size(438, 22);
+            this.gtadir_txt.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(438, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "GTA V Directory";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(438, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Copy MOD files to";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // copyto_txt
+            // 
+            this.copyto_txt.Location = new System.Drawing.Point(0, 121);
+            this.copyto_txt.Name = "copyto_txt";
+            this.copyto_txt.Size = new System.Drawing.Size(438, 22);
+            this.copyto_txt.TabIndex = 20;
+            // 
+            // select_bttn
+            // 
+            this.select_bttn.Location = new System.Drawing.Point(0, 62);
+            this.select_bttn.Name = "select_bttn";
+            this.select_bttn.Size = new System.Drawing.Size(438, 23);
+            this.select_bttn.TabIndex = 22;
+            this.select_bttn.Text = "SELECT";
+            this.select_bttn.UseVisualStyleBackColor = true;
+            this.select_bttn.Click += new System.EventHandler(this.select_bttn_Click);
+            // 
+            // select2_bttn
+            // 
+            this.select2_bttn.Location = new System.Drawing.Point(0, 149);
+            this.select2_bttn.Name = "select2_bttn";
+            this.select2_bttn.Size = new System.Drawing.Size(438, 23);
+            this.select2_bttn.TabIndex = 23;
+            this.select2_bttn.Text = "SELECT";
+            this.select2_bttn.UseVisualStyleBackColor = true;
+            this.select2_bttn.Click += new System.EventHandler(this.select2_bttn_Click);
+            // 
+            // reset_app_bttn
+            // 
+            this.reset_app_bttn.Location = new System.Drawing.Point(553, 368);
+            this.reset_app_bttn.Name = "reset_app_bttn";
+            this.reset_app_bttn.Size = new System.Drawing.Size(75, 23);
+            this.reset_app_bttn.TabIndex = 24;
+            this.reset_app_bttn.Text = "Reset App";
+            this.reset_app_bttn.UseVisualStyleBackColor = true;
+            this.reset_app_bttn.Click += new System.EventHandler(this.reset_app_bttn_Click);
+            // 
+            // panel_main
+            // 
+            this.panel_main.Controls.Add(this.label1);
+            this.panel_main.Controls.Add(this.gtadir_txt);
+            this.panel_main.Controls.Add(this.select2_bttn);
+            this.panel_main.Controls.Add(this.select_bttn);
+            this.panel_main.Controls.Add(this.copyto_txt);
+            this.panel_main.Controls.Add(this.label2);
+            this.panel_main.Location = new System.Drawing.Point(93, 102);
+            this.panel_main.Name = "panel_main";
+            this.panel_main.Size = new System.Drawing.Size(438, 174);
+            this.panel_main.TabIndex = 25;
+            // 
+            // folderBrowserDialog_temp
+            // 
+            this.folderBrowserDialog_temp.HelpRequest += new System.EventHandler(this.folderBrowserDialog_temp_HelpRequest);
+            // 
+            // settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.panel_main);
+            this.Controls.Add(this.reset_app_bttn);
+            this.Controls.Add(this.back_bttn);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold);
+            this.Name = "settings";
+            this.Size = new System.Drawing.Size(631, 394);
+            this.Load += new System.EventHandler(this.settings_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.back_bttn)).EndInit();
+            this.panel_main.ResumeLayout(false);
+            this.panel_main.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox back_bttn;
+        private System.Windows.Forms.TextBox gtadir_txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox copyto_txt;
+        private System.Windows.Forms.Button select_bttn;
+        private System.Windows.Forms.Button select2_bttn;
+        private System.Windows.Forms.Button reset_app_bttn;
+        private System.Windows.Forms.Panel panel_main;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog_temp;
+    }
+}
---
+++ GTA Mod Uninstaller/settings.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace GTA_Mod_Uninstaller
+{
+    public partial class settings : UserControl
+    {
+        Properties.Settings sett = new Properties.Settings();
+        static settings _obj;
+
+        public static settings Instance
+        {
+            get
+            {
+                if (_obj == null)
+                {
+                    _obj = new settings();
+                }
+                return _obj;
+            }
+        }
+
+        public Panel PnlContainerInfo
+        {
+            get { return panel_main; }
+            set { panel_main = value; }
+        }
+
+        public settings()
+        {
+            InitializeComponent();
+        }
+
+        private void settings_Load(object sender, EventArgs e)
+        {
+            _obj = this;
+            gtadir_txt.Text = sett.gtaPath;
+            copyto_txt.Text = sett.copyToPath;
+        }
+
+        private void back_bttn_Click(object sender, EventArgs e)
+        {
+            this.SendToBack();
+            this.Hide();
+            Form1 form = new Form1();
+            form.LoadListView();
+        }
+
+        private void reset_app_bttn_Click(object sender, EventArgs e)
+        {
+            sett.copyToPath = "";
+            sett.gtaPath = "";
+            sett.Save();
+            Process.Start(Application.ExecutablePath);
+            Application.Exit();
+        }
+
+        private void select_bttn_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.SelectedPath = sett.gtaPath;
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] fileTemp = Directory.GetFiles(folderBrowserDialog.SelectedPath);
+
+                for (int a = 0; a < fileTemp.Length; a++)
+                {
+                    string fileName = Path.GetFileName(fileTemp[a]);
+
+                    if (fileName.Contains("GTA5.exe"))
+                    {
+                        Console.WriteLine("GTA5.exe Finded!");
+                        sett.gtaPath = folderBrowserDialog.SelectedPath;
+                        gtadir_txt.Text = folderBrowserDialog.SelectedPath;
+                        sett.Save();
+                        Form1 form = new Form1();
+                        form.Success();
+                        RestartRequired();
+                        return;
+                    }
+                    else if (a == fileTemp.Length - 1)
+                    {
+                        MessageBox.Show("We don't find GTAV.exe!", "Ups...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+        private void select2_bttn_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog_temp.ShowDialog() == DialogResult.OK)
+            {
+                sett.copyToPath = folderBrowserDialog_temp.SelectedPath;
+                sett.Save();
+                copyto_txt.Text = folderBrowserDialog_temp.SelectedPath;
+                RestartRequired();
+            }
+        }
+
+        private void folderBrowserDialog_temp_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RestartRequired()
+        {
+            if (MessageBox.Show("We need to restart App!\nApp start automatically after this window close.", "Restart Required!", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+            {
+                Process.Start(Application.ExecutablePath);
+                Application.Exit();
+            }
+        }
+    }
+}
